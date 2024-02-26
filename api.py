f43de0fd88febe687876c70e690e053add5e2c9c
gonzagomezp <1912988@ucc.edu.ar>
2024-02-26 10:58:16 -0300

    LISTO

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import requests
+import simplejson
 
 app = FastAPI()
 
@@ -14,12 +15,20 @@ def fetch_external_api():
     try:
         response = requests.get("https://api.publicapis.org")
         response.raise_for_status()  # Raise an HTTPError for bad responses
-        data = response.json()
-        print("Response from external API:", data)
-        return data
+
+        if "application/json" in response.headers.get("content-type", ""):
+            data = response.json()
+            print("Response from external API:", data)
+            return data
+        else:
+            print("HTML Response from external API:", response.text)
+            return response.text
+        
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=f"Error fetching external API: {str(e)}")
-
+    except simplejson.JSONDecodeError as e:
+        raise HTTPException(status_code=500, detail=f"Error decoding JSON: {str(e)}")
+    
 # Endpoint UPDATE
 @app.put("/api/{number}")
 def square_number(number: int):
